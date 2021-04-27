c40c0dda86c2a32765380b9217dbc73639b8aa97
BattleUSAGI <531744708@qq.com>
2021-04-28 00:14:03 +0800

    更新

---
+++ 3D_Project/Assets/PlayerTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTest : MonoBehaviour
+{
+    public Rigidbody rb;
+    public bool bottonDown;
+
+
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ButtonDown();
+        if (bottonDown)
+        {
+            rb.AddForce(new Vector3(1,1,1) * 500f, ForceMode.Impulse);
+        }
+    }
+
+    private bool ButtonDown()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            Debug.Log("I pressed Z botton");
+            return bottonDown = true;
+        }
+        return bottonDown = false;
+    }
+
+
+
+
+
+
+
+
+
+}
